56cb223b57e611a2fee3c499f2461ea02edfbfb1
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-07-24 09:46:29 +0200

    I-1: Now we can start the Camunda appliation and have a process inside.

--- src/main/java/com/dpisarenko/camundarepl/CamundaReplApplication.java
+++ src/main/java/com/dpisarenko/camundarepl/CamundaReplApplication.java
@@ -1,11 +1,25 @@
 package com.dpisarenko.camundarepl;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class CamundaReplApplication {
+    @Autowired
+    private RuntimeService runtimeService;
+
     public static void main(String... args) {
         SpringApplication.run(CamundaReplApplication.class, args);
+    }
+
+    @EventListener
+    private void processPostDeploy(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("Process1");
     }
 }
