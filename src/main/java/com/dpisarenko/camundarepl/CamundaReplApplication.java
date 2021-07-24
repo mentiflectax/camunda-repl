057d053646efae522dccadc48e01538432c6cd77
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-07-24 09:28:04 +0200

    I-1: Setting up the Camunda foundation.

---
+++ src/main/java/com/dpisarenko/camundarepl/CamundaReplApplication.java
@@ -0,0 +1,11 @@
+package com.dpisarenko.camundarepl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaReplApplication {
+    public static void main(String... args) {
+        SpringApplication.run(CamundaReplApplication.class, args);
+    }
+}
